8902c56090aa71144fb14043a7cf532fe7abfda6
andheroe <freefunning@gmail.com>
2020-08-30 15:14:56 +0300

    remove debug

--- update_marketplace.py
+++ update_marketplace.py
@@ -4,6 +4,5 @@ import requests
 import yaml
 
 url = f"https://github_actions:{os.getenv('SEAMLESS_WEB_API_PASSWORD')}@{os.getenv('SEAMLESS_HOST')}/api/v1/marketplace"
-print(url)
 res = requests.post(url, json=yaml.load(open('table_of_contents.yml')))
 res.raise_for_status()
