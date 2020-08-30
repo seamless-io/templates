34bbf0bc56f384146d933b323553edaf5f9b0f8a
andheroe <freefunning@gmail.com>
2020-08-30 14:07:27 +0300

    added tags to yml file and name uniqueness check

--- rebuild_readme.py
+++ rebuild_readme.py
@@ -1,10 +1,23 @@
+from typing import List
+
 import yaml
 
 result = yaml.load(open('table_of_contents.yml'))
 content = """# Job templates
 
 """
+
+
+def check_name_uniqueness(names: List[str]):
+    names_set = set(names)
+    if len(names) != len(names_set):
+        raise RuntimeError('Template names should be unique!')
+
+
+names = []
 for template in result['templates']:
+    names.append(template['name'])
     content += f"- [{template['name']}]({template['long_description_path']}) - {template['short_description']}\n"
+check_name_uniqueness(names)
 with open('README.md', 'w') as f:
     f.write(content)
