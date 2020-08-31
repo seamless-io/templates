3c31bd04abf6da5ccbb48315b6a3c91d58a3d98d
andheroe <freefunning@gmail.com>
2020-08-31 21:35:13 +0300

    test

--- update_marketplace.py
+++ update_marketplace.py
@@ -1,8 +1,14 @@
 import os
+import tarfile
 
 import requests
-import yaml
+
+PACKAGE_NAME = 'templates.tar.gz'
+
+tar = tarfile.open(PACKAGE_NAME, "w:gz")
+tar.add('.')
+tar.close()
 
 url = f"https://github_actions:{os.getenv('SEAMLESS_WEB_API_PASSWORD')}@{os.getenv('SEAMLESS_HOST')}/api/v1/marketplace"
-res = requests.post(url, json=yaml.load(open('table_of_contents.yml')))
-res.raise_for_status()
+resp = requests.post(url, files={'templates': open(PACKAGE_NAME, 'rb')})
+resp.raise_for_status()
